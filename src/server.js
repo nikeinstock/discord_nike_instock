1f1fa02d0a381f705755375fb68d5624eec4bcc9
nikeinstock <122600956+nikeinstock@users.noreply.github.com>
2023-01-17 01:11:28 -0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ server.all(`/`, (req, res) => {
 });
 
 function keepAlive() {
-    server.listen(3000, () => {
+    server.listen(parseInt(process.env.PORT, 10), () => {
         console.log(`Server is now ready! | ` + Date.now());
     });
 }
